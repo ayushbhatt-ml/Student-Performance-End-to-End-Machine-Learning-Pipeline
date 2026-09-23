ad283e898b8cb9e621ef99c874463d5de8cebe87
ayushbhatt0 <ayushbhatt7417@gmail.com>
2026-09-23 13:16:20 +0530

    modify setup.py and add srs file in project folder

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+def get_requirements()-> List[str]:
+    requirement_lst : List[str]= []
+    try:
+        with open("requirements.txt","r") as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != "-e .":
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
-    return requirements
+    return requirement_lst
+
+
 
 setup(
 name='mlproject',
@@ -22,6 +23,6 @@ version='0.0.1',
 author='Ayush',
 author_email='ayushbhatt.ml@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+install_requires=get_requirements()
 
 )
